e89f6cc90e68f17b6ecc2076604cf6e0664f9f20
hoap-takada <takada@hoap-inc.jp>
2025-08-24 23:26:15 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-// before
-import '@/styles/globals.css'
+// pages/_app.js
+import '../styles/globals.css';
 
-// after
-import '../styles/globals.css'
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
