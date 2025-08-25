011107088bc4099362ffd62e39e4ceab8c1f6216
hoap-takada <takada@hoap-inc.jp>
2025-08-25 21:48:12 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
 // pages/_app.js
-import Head from "next/head";
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Noto+Sans+JP:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>ほーぷちゃん</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
