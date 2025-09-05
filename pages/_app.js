12e6c26e6c7469b01b03508c588a88df3e6f5392
hoap-takada <takada@hoap-inc.jp>
2025-09-05 11:58:45 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
 // pages/_app.js
 import "../styles/globals.css";
-import dynamic from "next/dynamic";
-
-const HoapChat = dynamic(() => import("../components/HoapChat"), { ssr: false });
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <HoapChat />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
